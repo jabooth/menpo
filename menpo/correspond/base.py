0bba27f48bbe8479c28989e3a1f509f017b6500b
James Booth <jabooth@gmail.com>
2014-07-12 19:49:16 +0100

    bits of functionality that need to be revised

---
+++ menpo/correspond/base.py
@@ -0,0 +1,222 @@
+from menpo.transform import GeneralizedProcrustesAnalysis
+from menpo.model import PCAModel
+
+from .flatinterp import TriMeshCorresponder
+
+
+class MMBuilder(object):
+
+    def __init__(self, models, group=None, label='all', **kwargs):
+        self.models = models
+        self.group = group
+        self.label = label
+        gpa = GeneralizedProcrustesAnalysis([m.landmarks[group][label].lms
+                                             for m in self.models])
+        self.target = gpa.mean_aligned_shape
+        self.corresponder = TriMeshCorresponder(self.target, **kwargs)
+        self.shape_model = None
+        self.dc_models = None
+
+    def correspond(self):
+        self.dc_models = [self.corresponder.correspond(m, group=self.group,
+                                                       label=self.label)
+                          for m in self.models]
+
+    def pca(self):
+        self.shape_model = PCAModel(self.dc_models)
+
+
+def sample_points_and_trilist(mask, sampling_rate=1):
+    r"""
+    Returns sampling indices in x and y along with a trilist that joins the
+    together the points.
+
+    Parameters
+    ----------
+
+    mask: :class:`BooleanImage`
+        The mask that should be sampled from
+
+    sampling_rate: integer, optional
+        The spacing of the grid
+
+        Default 1
+
+    Returns
+    -------
+
+    sample_x, sample_y, trilist: All ndarray
+        To be used on a pixel array of an shape Image as follows
+        points = img.pixels[sample_x, sample_y, :]
+        points and trilist are now useful to construct a TriMesh
+
+    """
+    x, y = np.meshgrid(np.arange(0, mask.height, sampling_rate),
+                       np.arange(0, mask.width, sampling_rate))
+    # use the mask's mask to filter which points should be
+    # selectable
+    sampler_in_mask = mask.mask[x, y]
+    sample_x, sample_y = x[sampler_in_mask], y[sampler_in_mask]
+    # build a cheeky TriMesh to get hassle free trilist
+    tm = TriMesh(np.vstack([sample_x, sample_y]).T)
+    return sample_x, sample_y, tm.trilist.copy()
+
+
+def prune_trilist(radius, cy_unwrapped, dead_zone=0.1):
+    t = cy_unwrapped.trilist
+    bound = (1 - dead_zone) * np.pi * radius
+    x = cy_unwrapped.points[:, 0]
+    # find which vertices are in the left and right
+    lhs, rhs = np.where(x < - bound)[0], np.where(x > bound)[0]
+    in_set = lambda tl, s: np.in1d(tl.ravel(), s).reshape([-1, 3]).any(axis=1)
+    bad_tris = np.logical_and(in_set(t, lhs), in_set(t, rhs))
+    return t[~bad_tris]
+
+
+def image_to_tcoords(shape, a_0, a_1):
+    return np.hstack([(a_1 * 1.0 / shape[1])[:, None],
+                      ((shape[0] - a_0) * 1.0 /shape[0])[:, None]])
+
+
+class TriMeshMapSampler(object):
+    r"""
+    Object for bringing landmarked TriMeshes into dense correspondence by
+    flattening the mesh into a 2D atlas and performing an interpolation in
+    the 2D space. The resultant interpolated shape is sampled at a set interval
+    producing a mesh with a particular number of points, particular
+    connectivity, and particular semantic meaning assigned to each point.
+
+    In other words, if a set of TriMeshes is passed through a particular
+    :class:`TriMeshCorresponder`, the returned the resulting meshes
+    will be in dense correspondence.
+
+    Parameters
+    ----------
+
+    target: :class:`PointCloud`
+        The landmarks that input :class:`TriMesh` landmarks will be aligned
+        against
+
+    interpolator: :class:`Alignment`
+        The 2D alignment that should be used to interpolate the flattened
+        meshes
+
+    shape_image_width: integer
+        The desired width of the shape_image in pixels. Note that the exact
+        width may differ slightly. The height of the shape image will be
+        automatically selected based on the ratio of the flattened target.
+
+    """
+    def __init__(self, target, interpolator=ThinPlateSplines,
+                 image_width=1000, clip_space_scale=0.9,
+                 mask_dilation=20, sampling_rate=1,
+                 cy_dead_zone=0.1):
+
+        self.f_interp = FlattenInterp(target, interp=interpolator,
+                                      cy_dead_zone=cy_dead_zone)
+
+        # 1. RASTERIZE
+
+        # Need to decide the appropriate size of the image - check the
+        # ratio of the flatted 2D mesh and use it to infer height
+        r_h, r_w = self.f_interp.f_tgt_2d.range()
+        ratio_w_to_h = (1.0 * r_w) / r_h
+        image_height = int(ratio_w_to_h * image_width)
+
+        # Build the rasterizer providing the clip space transform and shape
+        cs_transform = model_to_clip_transform(self.f_interp.f_tgt_3d,
+                                               xy_scale=clip_space_scale)
+        self.r = GLRasterizer(projection_matrix=cs_transform.h_matrix,
+                              width=image_width,
+                              height=image_height)
+
+        # 2. SAMPLING
+
+        # Save out where the target landmarks land in the image
+        self.img_tgt_2d = self.r.model_to_image_transform.apply(
+            self.f_interp.f_tgt_3d)
+
+        # make an example of the output we expect, and attach the lms
+        sample_output = MaskedImage.blank((self.r.height, self.r.width))
+        sample_output.landmarks['correspondence_target'] = self.img_tgt_2d
+        # create the mask we will want and save it for attaching on outputs
+        sample_output.constrain_mask_to_landmarks()
+        if mask_dilation != 0:
+            self.mask = BooleanImage(binary_dilation(sample_output.mask.mask,
+                                                     iterations=mask_dilation))
+        else:
+            self.mask = sample_output.mask
+        self.mask.landmarks = sample_output.landmarks
+
+        self.sampling_rate = sampling_rate
+        # attach the sampling arrays
+        self.s_x, self.s_y, self.trilist = sample_points_and_trilist(
+            self.mask, self.sampling_rate)
+        self.tcoords = image_to_tcoords(self.mask.shape, self.s_x, self.s_y)
+
+    def correspond(self, mesh, group=None, label='all'):
+        r"""
+        Return a version of the mesh that has been placed in correspondence
+        by the rules of this TriMeshCorresponder.
+
+        Parameters
+        ----------
+
+        mesh : :class:`TriMesh`
+            The mesh that should be placed in correspondence
+        group: string, optional
+            The group label of the landmarks that should be used for
+            interpolation. By default None, which will select the only
+            available group if only one landmark group is present
+
+            Default None
+        label: string, optional
+            The label of a particular set of landmarks, e.g. 'left_eye'.
+
+            Default 'all'
+
+        Returns
+        -------
+
+        :class:`TriMesh` A trimesh in correspondence with this corresponder.
+        """
+        return self.generate_correspondence_mesh(*self.f_interp(mesh,
+                                                                group=group,
+                                                                label=label))
+
+    def generate_correspondence_mesh(self, f_interp_mesh, mesh):
+        r"""
+        Use a flattened warped mesh to build back a TriMesh in dense
+        correspondence.
+
+        Parameters
+        ----------
+
+        mesh: :class:`TriMesh`
+            The original (rigidly aligned) TriMesh that we want to place in
+            dense correspondence
+
+        flattened_warped_mesh: :class:`TriMesh`
+
+
+        """
+        # build the rgb and shape images
+        rgb_image, shape_image = self.r.rasterize_mesh_with_f3v_interpolant(
+            f_interp_mesh, per_vertex_f3v=mesh.points)
+        # rgb_image.mask = self.mask.copy()
+        # shape_image.mask = self.mask.copy()
+        if isinstance(mesh, TexturedTriMesh):
+            tm = self._extract_textured_trimesh(shape_image, rgb_image)
+        else:
+            tm = self._extract_trimesh(shape_image)
+        tm.landmarks = mesh.landmarks
+        return tm, rgb_image, shape_image
+
+    def _extract_trimesh(self, shape_image):
+        sampled = shape_image.pixels[self.s_x, self.s_y, :]
+        return TriMesh(sampled, trilist=self.trilist)
+
+    def _extract_textured_trimesh(self, shape_image, rgb_image):
+        sampled = shape_image.pixels[self.s_x, self.s_y, :]
+        return TexturedTriMesh(sampled, self.tcoords, rgb_image,
+                               trilist=self.trilist)
