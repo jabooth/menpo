18c447e7632da4ed26dea9fcd94f001d6f54f643
James Booth <jabooth@gmail.com>
2014-09-06 15:52:03 +0100

    rearrange aam builder imports

--- menpo/fitmultilevel/aam/builder.py
+++ menpo/fitmultilevel/aam/builder.py
@@ -1,18 +1,16 @@
 from __future__ import division
 import numpy as np
 
-from menpo.shape import TriMesh
-from menpo.image import MaskedImage
-from menpo.transform import Translation
-from menpo.transform.piecewiseaffine import PiecewiseAffine
-from menpo.transform.thinplatesplines import ThinPlateSplines
-from menpo.model import PCAModel
+from menpo.fitmultilevel import checks
 from menpo.fitmultilevel.builder import (DeformableModelBuilder,
                                          normalization_wrt_reference_shape,
                                          build_shape_model, create_pyramid)
-from menpo.fitmultilevel import checks
-from menpo.visualize import print_dynamic, progress_bar_str
 from menpo.feature import igo
+from menpo.image import MaskedImage
+from menpo.model import PCAModel
+from menpo.shape import TriMesh
+from menpo.transform import Translation, PiecewiseAffine, ThinPlateSplines
+from menpo.visualize import print_dynamic, progress_bar_str
 
 
 class AAMBuilder(DeformableModelBuilder):
